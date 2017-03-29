f7b616b272ef02802aa271620b4dbeb5eb81a910
SenorTaylor <TaylorJohnsonRHS@gmail.com>
2017-03-29 12:20:30 -0400

    Add files via upload

---
+++ roster_admin.php
@@ -0,0 +1,406 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Roster</title>
+
+
+    <!-- Bootstrap CSS -->    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- bootstrap theme -->
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <!--external css-->
+    <!-- font icon -->
+    <link href="css/elegant-icons-style.css" rel="stylesheet" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" />    
+    <!-- full calendar css-->
+    <link href="assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
+    <link href="assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
+    <!-- easy pie chart-->
+    <link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <link href="css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="css/fullcalendar.css">
+    <link href="css/widgets.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+    <link href="css/xcharts.min.css" rel=" stylesheet"> 
+    <link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+
+      <link href="css/custom.css" rel="stylesheet" type="text/css" media="screen">
+    <link rel="stylesheet" href="static/stylesheets/app.css">
+<script src="static/scripts/library.js"></script>
+<script src="static/scripts/base.js"></script>
+<script src="static/scripts/chart.js"></script>
+<script src="static/scripts/initialize.js"></script>
+
+
+
+
+    <link href="custom.css" rel="stylesheet" type="text/css" media="screen">
+
+    <!-- Custom CSS -->
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div id="header">
+   <ul class="con">
+    <div id="header2">
+        <li> 
+           <a href="profile.php">Jennifer Alba<img src="img/profile-widget-avatar.jpg" height=30px width=30px></a>
+         </li>
+           <li id="settings">
+           <a href="profile_settings.php"><img src="settings.png" class="img-responsive"></a>
+       </li>
+       <li id="exit">
+   <a href="/484/log-in/login_2.php"><img src="exit.png" class="img-responsive"></a>
+       </li>               
+
+   </ul>
+</div>
+
+
+
+
+
+<!-- NAVIGATION BAR -->
+           
+<div id="stream">
+   <div class="con">
+       <div class="tile" id="hello">
+        <div class="col-sm-12">
+         <h2><span>Hi,</span> Jennifer</h2>
+          <p>You last visited <strong>two hours</strong> ago</p>
+       </div>
+   </div>
+   <div>
+ </div>
+
+
+
+  <div class="row">
+    <div class="container">
+        <ul id="nav" class="nav">
+        <li> 
+           <a  class="tile" href="index_admin.php"> 
+              <span class="vector"><img src="home.png" class="img-responsive"></span>
+           <span class="title"><strong>Home</strong></span>
+         </a>
+         </li>
+
+            <li> 
+           <a  class="tile" href="roster_admin.php"> 
+              <span class="vector"><img src="group.png" class="img-responsive"></span>           
+              <span class="title"><strong>Roster</strong></span>
+         </a>
+         </li>
+
+        <li> 
+           <a  class="tile" href="dept_admin.php"> 
+
+              <span class="vector"><img src="edit.png" class="img-responsive"></span>
+           <span class="title"><strong>Requirements</strong></span>
+         </a>
+         </li>  
+
+          <li> 
+           <a  class="tile" href="shifts_admin.php">
+              <span class="vector"><img src="calendar.png" class="img-responsive"></span>
+           <span class="title"><strong>Shifts</strong></span>
+         </a>
+         </li> 
+
+          <li> 
+           <a  class="tile" href="applications.php"> 
+              <span class="vector"><img src="resume.png" class="img-responsive"></span>
+           <span class="title"><strong>Applications</strong></span>
+         </a>
+         </li>   
+
+
+   </ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+  <div id="dashboard">
+      <div class="scroll con">
+          <div class="section padding current" title="Tables" id="table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Department</th>
+                  <th style="width: 200px">Total Hours</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Arnulfo Derico</td>
+                  <td>Neue <strong>Content</strong></td>
+                  <td><span class="chrome">Chrome</span></td>
+                </tr>
+                <tr>
+                  <td>Micha Mahomes</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="firefox">Firefox</span></td>
+
+                </tr>
+                <tr>
+                  <td>Aldo Maycumber</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+                
+                </tr>
+                <tr>
+                  <td>Sydney Schwarm</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="ie">Internet Explorer</span></td>
+  
+                </tr>
+                
+                <tr>
+                  <td>Thad Wemhoff</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Elmo Kostis</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Wanetta Herne</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+
+                </tr>
+                <tr>
+                  <td>Chance Podany</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="firefox">Firefox</span></td>
+
+                </tr>
+                <tr>
+                  <td>Emilia Thaler</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="ie">Internet Explorer</span></td>
+
+                </tr>
+                <tr>
+                  <td>Pearlie Ruther</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Zelma Roosevelt</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+
+                </tr>
+                <tr>
+                  <td>Clayton Dano</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="firefox">Firefox</span></td>
+
+                </tr>
+                <tr>
+                  <td>Kurt Bomba</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+               
+                </tr>
+                <tr>
+                  <td>Annabelle Morein</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="ie">Internet Explorer</span></td>
+
+                </tr>
+                
+                <tr>
+                  <td>Noemi Hux</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Tanisha Houtchens</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Malinda Brick</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+
+                </tr>
+                <tr>
+                  <td>Julio Yeldell</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="firefox">Firefox</span></td>
+
+                </tr>
+                <tr>
+                  <td>Nita Brion</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="ie">Internet Explorer</span></td>
+    
+                </tr>
+                <tr>
+                  <td>Lakeesha Wactor</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Sharron Wheelwright</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+
+                </tr>
+                <tr>
+                  <td>Erik Mayville</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="firefox">Firefox</span></td>
+
+                </tr>
+                <tr>
+                  <td>Carmella Coffer</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+              
+                </tr>
+                <tr>
+                  <td>Malinda Phegley</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="ie">Internet Explorer</span></td>
+
+                </tr>
+                
+                <tr>
+                  <td>Max Bibler</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Roslyn Glatt</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Javier Gau</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+
+                </tr>
+                <tr>
+                  <td>Marcie Dahlen</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="firefox">Firefox</span></td>
+
+                </tr>
+                <tr>
+                  <td>Lance Sabourin</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="ie">Internet Explorer</span></td>
+
+                </tr>
+                <tr>
+                  <td>Julianne Mahle</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="safari">Safari</span></td>
+
+                </tr>
+                <tr>
+                  <td>Jacques Halifax</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+
+                </tr>
+                <tr>
+                  <td>Alana Nylander</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="firefox">Firefox</span></td>
+
+                </tr>
+                <tr>
+                  <td>Sofia Deveau</td>
+                  <td>row 2, cell 2</td>
+                  <td><span class="chrome">Chrome</span></td>
+             
+                </tr>
+                
+              </tbody>
+            </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </div>
+      </div>
+  </div>
+  <div id="footer">
+      <div class="con">
+     
+      </div>
+  </div>
+</body>
+</html>
+
+
+
+
+
+
+
+    </div>
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
